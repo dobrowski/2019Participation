318bace90aa3e3090bfa2a53be409e0c9aac6905
David Dobrowski <dobrowski@gmail.com>
2020-01-06 10:15:52 -0800

    Init

---
+++ 2019Participation.R
@@ -0,0 +1,42 @@
+
+
+library(here)
+library(tidyverse)
+library(readxl)
+
+
+### Load data ------
+
+
+math <- read_excel(here("data","mathpratedownload2019.xlsx") )
+
+ela <- read_excel(here("data","elapratedownload2019.xlsx") )
+
+
+elpac <- read_excel(here("data","elpacpart2019.xlsx") )
+
+### Clean and merge ----
+
+elpac <- elpac %>%
+    mutate(studentgroup = "EL") %>%
+    mutate(test = "elpac")
+
+math <- math %>%
+    mutate(test = "math")
+
+ela <- ela %>%
+    mutate(test = "ela")
+
+participation <- math %>%
+    bind_rows(ela) %>%
+    bind_rows(elpac) %>%
+    mutate(prate = as.numeric(prate))
+
+
+participation.mry <- participation %>%
+    filter(countyname == "Monterey")
+
+participation.mry.low <- participation.mry %>%
+    filter(prate < 95)
+
+
